b15bec3cb868bb64b191bfeb94ca61d5d195f078
James Potter <james.potter@gmail.com>
2012-03-14 21:12:52 +0000

    Complete styling for button element

--- ios/CBButtonViewController.h
+++ ios/CBButtonViewController.h
@@ -6,13 +6,20 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
+
 #import "CBElementViewController.h"
 #import "CBButton.h"
 
-@interface CBButtonViewController : CBElementViewController
+@interface CBButtonViewController : CBElementViewController {
+    @private
+    bool _enabled;
+    UILabel *_buttonLabel;
+}
 
 - (id)initWithButton:(CBButton *)button;
 
 @property (nonatomic, strong, readonly) CBButton *button;
+@property (nonatomic, assign) bool enabled;
 
 @end
--- ios/CBButtonViewController.m
+++ ios/CBButtonViewController.m
@@ -11,22 +11,59 @@
 @implementation CBButtonViewController
 
 @synthesize button;
+@synthesize enabled=_enabled;
 
 - (id)initWithButton:(CBButton *)theButton
 {
     self = [super init];
     if (self) {
         button = theButton;
+        _enabled = NO;
     }
     return self;
 }
 
 - (void)loadView
 {
-    UIView *v = [[UIView alloc] init];
-    v.backgroundColor = [UIColor greenColor];
+    UIView *v = [[UIView alloc] initWithFrame:CGRectZero];
+    v.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
     
+    _buttonLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+    _buttonLabel.text = self.button.label;
+    _buttonLabel.textAlignment = UITextAlignmentCenter;
+    _buttonLabel.backgroundColor = [UIColor clearColor];
+    _buttonLabel.textColor = labelColor;
+    
+    [v addSubview:_buttonLabel];
     self.view = v;
+    
+    // setEnabled handles the remaining styling
+    [self setEnabled:NO];
+}
+
+- (void)viewDidLayoutSubviews
+{
+    // Fill the view, so text is centered
+    _buttonLabel.frame = self.view.bounds;
+}
+
+- (bool)enabled
+{
+    return _enabled;
+}
+
+- (void)setEnabled:(bool)enabled
+{
+    _enabled = enabled;
+    
+    CGFloat fontSize = 24.0f;
+    if (enabled) {
+        self.view.backgroundColor= activeColor;
+        _buttonLabel.font = [UIFont boldSystemFontOfSize:fontSize];
+    } else {
+        self.view.backgroundColor = inactiveColor;
+        _buttonLabel.font = [UIFont systemFontOfSize:fontSize];
+    }
 }
 
 - (void)viewDidUnload
