2383a6812fe100d25fbbcefda2fabc87b6bdf417
James Potter <james.potter@gmail.com>
2012-04-29 00:54:21 +0100

    Basic circle for accelerator view

--- ios/CBAcceleratorView.m
+++ ios/CBAcceleratorView.m
@@ -14,9 +14,24 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        self.backgroundColor = [UIColor blueColor];
+        self.backgroundColor = [UIColor whiteColor];
     }
     return self;
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGFloat w = rect.size.width / 2;
+    CGFloat h = rect.size.height / 2;
+    CGFloat x = rect.origin.x + (w / 2);
+    CGFloat y = rect.origin.y + (h / 2);
+    
+    CGContextAddEllipseInRect(context, CGRectMake(x, y, w, h));
+    CGContextSetFillColorWithColor(context, [UIColor colorWithRed:1 green:0 blue:0 alpha:0.3].CGColor);
+    CGContextFillPath(context);
+    CGContextStrokePath(context);
 }
 
 @end
